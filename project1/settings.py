2d812121ff8b2ff9cad07434c682387dc97fb51c
Rakshit Halijol <90215747+Rakshithalijol123@users.noreply.github.com>
2023-04-26 19:50:35 +0530

    Update settings.py

--- project1/settings.py
+++ project1/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-ppap#2(lb&q&0n(pl+bhk#wn7hz0g_ur7i#ih+-6#*_*!@2!-1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
